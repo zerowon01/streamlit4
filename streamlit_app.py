19566b3113de7f4d22882d10fef7393ce56441eb
bloomg <greig.bloom@ipsoft.com>
2024-01-12 14:40:53 +0900

    Started the i18n push

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,61 @@
 import streamlit as st
+import gettext
 
-st.write("My app")
-st.balloons()
-st.write("A new update..")
+
+#i18n
+t = gettext.translation("i18n", "locale", fallback=True)
+_ = t.gettext
+
+
+#Sidebar
 st.sidebar.image("static/datarobot.png")
 
-st.write(st.experimental_user)
+#Body
+st.write("My app")
+st.write(_("Hello friends"))
+# st.balloons()
+st.write("A new update..")
+st.caption("Not sure about this either . . ")
+st.text("This is some text . . . . ")
+
+
+st.markdown(
+    """
+    <style>
+    .element-container:has(style){
+        display: none;
+    }
+    #button-after {
+        display: none;
+    }
+    .element-container:has(#button-two){
+    display: none;
+    }
+    .element-container:has(#button-after) {
+        display: none;
+    }
+    .element-container:has(#button-two) + div button{
+        background-color: green;
+        width:  200px;
+        height: 200px;
+    }
+    .element-container:has(#button-after) + div button {
+        background-color: orange;
+        width:  200px;
+        height: 200px;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+col1, col2 = st.columns([1,1])
+with col1:
+    st.markdown('<span id="button-after"></span>', unsafe_allow_html=True)
+    st.button("My Button")
+with col2:
+    st.markdown('<span id="button-two"></span>', unsafe_allow_html=True)
+    st.button("Some other button")
+
+
+# st.write(st.experimental_user)
